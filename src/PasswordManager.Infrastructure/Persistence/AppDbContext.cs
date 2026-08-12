bd6a87a808e21676f964a838f75a30272e0046d3
Dogukan <dogukanuckan@gmail.com>
2026-08-12 13:26:19 +0300

    Fix MSB3277 issues

---
+++ src/PasswordManager.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using PasswordManager.Domain.Entities;
+
+namespace PasswordManager.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<VaultItem> VaultItems => Set<VaultItem>();
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
