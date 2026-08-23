0cc8e34cdd50b0a4f0853d2d0b1fcabec1179a3b
Dogukan <dogukanuckan@gmail.com>
2026-08-23 21:54:41 +0300

    Register DbContext, repositories, and services in DI container

--- src/PasswordManager.API/Program.cs
+++ src/PasswordManager.API/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PasswordManager.Application.Interfaces.Repositories;
+using PasswordManager.Application.Interfaces.Services;
 using PasswordManager.Application.Options;
+using PasswordManager.Application.Services;
+using PasswordManager.Infrastructure.Persistence;
+using PasswordManager.Infrastructure.Repositories;
+using PasswordManager.Infrastructure.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +15,16 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.Configure<AuthOptions>(builder.Configuration.GetSection("Auth"));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+builder.Services.AddScoped<IVaultItemRepository, VaultItemRepository>();
+
+builder.Services.AddScoped<IPasswordHasher, Argon2PasswordHasher>();
+builder.Services.AddScoped<ITokenGenerator, JwtTokenGenerator>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
